20aca92e13c9eed0138ef0fa1eee9519794428e1
Antti Leppä <antti.leppa@foyt.fi>
2016-09-20 06:29:13 +0300

    Added support for manupulating organization settings via REST

--- src/main/java/fi/otavanopisto/kuntaapi/server/settings/OrganizationSettingController.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/settings/OrganizationSettingController.java
@@ -1,5 +1,7 @@
 package fi.otavanopisto.kuntaapi.server.settings;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.Dependent;
@@ -14,11 +16,13 @@ import fi.otavanopisto.kuntaapi.server.persistence.model.OrganizationSetting;
 /**
  * Controller for organization settings. Class does not handle concurrency so caller must take care of that
  * 
- * @author Otavan Opisto
+ * @author Antti Leppä
  */
 @Dependent
 public class OrganizationSettingController {
   
+  private static final String FAILED_TO_TRANSLATE = "Failed to translate %s into KuntaApiId id";
+
   @Inject
   private Logger logger;
 
@@ -41,27 +45,16 @@ public class OrganizationSettingController {
   public String getSettingValue(OrganizationId organizationId, String key) {
     OrganizationId kuntaApiId = idController.translateOrganizationId(organizationId, KuntaApiConsts.IDENTIFIER_NAME);
     if (kuntaApiId != null) {
-      return getSettingValue(kuntaApiId.getId(), key);
+      OrganizationSetting organizationSetting = organizationSettingDAO.findByKeyAndOrganizationKuntaApiId(key, kuntaApiId.getId());
+      if (organizationSetting != null) {
+        return organizationSetting.getValue();
+      }
+      
+      return null;
     } else {
-      logger.severe(String.format("Failed to translate %s into KuntaApiId id", organizationId.toString()));
+      logger.severe(String.format(FAILED_TO_TRANSLATE, organizationId.toString()));
       return null;
     }
-  }
-  
-  /**
-   * Returns organization setting by key or null if setting is not defined
-   * 
-   * @param kuntaApiId Kunta API id of organization
-   * @param key system setting key
-   * @return setting value
-   */
-  public String getSettingValue(String kuntaApiId, String key) {
-    OrganizationSetting organizationSetting = organizationSettingDAO.findByKeyAndOrganizationKuntaApiId(key, kuntaApiId);
-    if (organizationSetting != null) {
-      return organizationSetting.getValue();
-    }
-    
-    return null;
   }
   
   /**
@@ -69,15 +62,89 @@ public class OrganizationSettingController {
    * 
    * @param key system setting key
    * @param value new setting value
-   * @param organizationKuntaApiId 
+   * @param organizationId organization's id
    */
-  public void setSettingValue(String organizationKuntaApiId, String key, String value) {
-    OrganizationSetting organizationSetting = organizationSettingDAO.findByKeyAndOrganizationKuntaApiId(key, organizationKuntaApiId);
-    if (organizationSetting != null) {
-      organizationSettingDAO.updateValue(organizationSetting, value);
+  public void setSettingValue(OrganizationId organizationId, String key, String value) {
+    OrganizationId kuntaApiId = idController.translateOrganizationId(organizationId, KuntaApiConsts.IDENTIFIER_NAME);
+    if (kuntaApiId == null) {
+      logger.severe(String.format(FAILED_TO_TRANSLATE, organizationId.toString()));
     } else {
-      organizationSettingDAO.create(key, value, organizationKuntaApiId);
+      OrganizationSetting organizationSetting = organizationSettingDAO.findByKeyAndOrganizationKuntaApiId(key, kuntaApiId.getId());
+      if (organizationSetting != null) {
+        organizationSettingDAO.updateValue(organizationSetting, value);
+      } else {
+        createOrganizationSetting(key, value, kuntaApiId);
+      }
     }
+  }
+  
+  /**
+   * Creates a new organization setting
+   * 
+   * @param key key
+   * @param value value 
+   * @param organizationKuntaApiId organization's Kunta API Id
+   * @return OrganizationSetting
+   */
+  public OrganizationSetting createOrganizationSetting(String key, String value, OrganizationId organizationId) {
+    OrganizationId kuntaApiId = idController.translateOrganizationId(organizationId, KuntaApiConsts.IDENTIFIER_NAME);
+    if (kuntaApiId == null) {
+      logger.severe(String.format(FAILED_TO_TRANSLATE, organizationId.toString()));
+      return null;
+    } else {
+      return organizationSettingDAO.create(key, value, organizationId.getId());
+    } 
+  }
+  
+  /**
+   * Updates an organization setting
+   * 
+   * @param organizationSetting setting
+   * @param value value 
+   * @return OrganizationSetting
+   */
+  public OrganizationSetting updateOrganizationSetting(OrganizationSetting organizationSetting, String value) {
+    if (organizationSetting == null) {
+      logger.severe("Unable to update null setting");
+      return null;
+    }
+    
+    return organizationSettingDAO.updateValue(organizationSetting, value);
+  }
+  
+  /**
+   * Lists organization's keys
+   * 
+   * @param organizationKuntaApiId organization's Kunta API id
+   * @return list of organization's keys
+   */
+  public List<OrganizationSetting> listOrganizationSettings(OrganizationId organizationId) {
+    OrganizationId kuntaApiId = idController.translateOrganizationId(organizationId, KuntaApiConsts.IDENTIFIER_NAME);
+    if (kuntaApiId == null) {
+      logger.severe(String.format(FAILED_TO_TRANSLATE, organizationId.toString()));
+      return Collections.emptyList();
+    } else {
+      return organizationSettingDAO.listByOrganizationKuntaApiId(kuntaApiId.getId());
+    }
+  }
+  
+  /**
+   * Finds organization setting by id
+   * 
+   * @param id organization setting id
+   * @return organization setting or null if not found
+   */
+  public OrganizationSetting findOrganizationSetting(String id) {
+    return organizationSettingDAO.findById(id);
+  }
+
+  /**
+   * Deletes an organization setting
+   * 
+   * @param organizationSetting setting
+   */
+  public void deleteOrganizationSetting(OrganizationSetting organizationSetting) {
+    organizationSettingDAO.delete(organizationSetting);
   }
   
 }
