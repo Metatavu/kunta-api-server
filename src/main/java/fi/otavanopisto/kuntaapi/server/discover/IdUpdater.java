f56cb920653534d9a429e03cd086e291452be3d8
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-13 12:45:12 +0300

    Added generic base for discovering ids and entities

---
+++ src/main/java/fi/otavanopisto/kuntaapi/server/discover/IdUpdater.java
@@ -0,0 +1,11 @@
+package fi.otavanopisto.kuntaapi.server.discover;
+
+public interface IdUpdater {
+  
+  public void startTimer();
+  
+  public void stopTimer();
+  
+  public String getName();
+  
+}
