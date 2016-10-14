2b1690c2e3b703bb29a09652d626acba8e517aef
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-14 09:44:03 +0300

    Refactored ServiceChannelProvider to match new spec

--- src/main/java/fi/otavanopisto/kuntaapi/server/integrations/ServiceChannelProvider.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/ServiceChannelProvider.java
@@ -2,7 +2,11 @@ package fi.otavanopisto.kuntaapi.server.integrations;
 
 import java.util.List;
 
-import fi.otavanopisto.kuntaapi.server.rest.model.ServiceElectronicChannel;
+import fi.otavanopisto.kuntaapi.server.rest.model.ElectronicChannel;
+import fi.otavanopisto.kuntaapi.server.rest.model.PhoneChannel;
+import fi.otavanopisto.kuntaapi.server.rest.model.PrintableFormChannel;
+import fi.otavanopisto.kuntaapi.server.rest.model.ServiceLocationChannel;
+import fi.otavanopisto.kuntaapi.server.rest.model.WebPageChannel;
 
 /**
  * Provider for service channels
@@ -18,6 +22,42 @@ public interface ServiceChannelProvider {
    * @param serviceId serviceId
    * @return List of service electronic channels
    */
-  public List<ServiceElectronicChannel> listElectronicChannels(OrganizationId organizationId, ServiceId serviceId);
-
+  public List<ElectronicChannel> listElectronicChannels(OrganizationId organizationId, ServiceId serviceId);
+  
+  /**
+   * Lists service phone channels
+   * 
+   * @param organizationId organization id
+   * @param serviceId serviceId
+   * @return List of service phone channels
+   */
+  public List<PhoneChannel> listPhoneChannels(OrganizationId organizationId, ServiceId serviceId);
+  
+  /**
+   * Lists service printable form channels
+   * 
+   * @param organizationId organization id
+   * @param serviceId serviceId
+   * @return List of service printable form channels
+   */
+  public List<PrintableFormChannel> listPrintableFormChannels(OrganizationId organizationId, ServiceId serviceId);
+  
+  /**
+   * Lists service location channels
+   * 
+   * @param organizationId organization id
+   * @param serviceId serviceId
+   * @return List of service location channels
+   */
+  public List<ServiceLocationChannel> listServiceLocationChannels(OrganizationId organizationId, ServiceId serviceId);
+  
+  /**
+   * Lists service webpage channels
+   * 
+   * @param organizationId organization id
+   * @param serviceId serviceId
+   * @return List of service webpage channels
+   */
+  public List<WebPageChannel> listWebPageChannelsChannels(OrganizationId organizationId, ServiceId serviceId);
+  
 }
