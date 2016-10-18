39cf56045f56c75ef389e6363701c4c852e9d75c
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-18 09:25:49 +0300

    Fixed sonar qube issues

--- src/main/java/fi/otavanopisto/kuntaapi/server/discover/EntityUpdater.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/discover/EntityUpdater.java
@@ -1,6 +1,6 @@
 package fi.otavanopisto.kuntaapi.server.discover;
 
-@SuppressWarnings("squid:S1610 ")
+@SuppressWarnings("squid:S1610")
 public abstract class EntityUpdater {
   
   public abstract void startTimer();
