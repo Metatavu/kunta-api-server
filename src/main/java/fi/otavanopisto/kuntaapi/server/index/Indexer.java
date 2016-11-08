e6b827cf2203bc168a5072b50464b5061b4d5ff8
Antti Leppä <antti.leppa@metatavu.fi>
2016-11-08 14:19:34 +0200

    Fixed transaction issue on Indexer

--- src/main/java/fi/otavanopisto/kuntaapi/server/index/Indexer.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/index/Indexer.java
@@ -4,6 +4,8 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.enterprise.event.TransactionPhase;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
 
 @ApplicationScoped
 public class Indexer {
@@ -11,6 +13,7 @@ public class Indexer {
   @Inject
   private IndexUpdater indexUpdater;
 
+  @Transactional (TxType.REQUIRES_NEW)
   public void onIndexRequest(@Observes (during = TransactionPhase.AFTER_COMPLETION) IndexRequest indexRequest) {
     Indexable indexable = indexRequest.getIndexable();
     indexUpdater.index(indexable);
