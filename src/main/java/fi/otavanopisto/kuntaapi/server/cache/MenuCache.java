5597d6bf077261c172b6dad8ed86af5020018c4e
Antti Leppä <antti.leppa@metatavu.fi>
2016-11-17 09:20:27 +0200

    Chnged menu itesms to be loaded from the cache

---
+++ src/main/java/fi/otavanopisto/kuntaapi/server/cache/MenuCache.java
@@ -0,0 +1,18 @@
+package fi.otavanopisto.kuntaapi.server.cache;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import fi.otavanopisto.kuntaapi.server.id.MenuId;
+import fi.otavanopisto.kuntaapi.server.rest.model.Menu;
+
+@ApplicationScoped
+public class MenuCache extends AbstractEntityCache<MenuId, Menu> {
+  
+  private static final long serialVersionUID = 7116646256910860395L;
+
+  @Override
+  public String getCacheName() {
+    return "menus";
+  }
+  
+}
--- src/main/java/fi/otavanopisto/kuntaapi/server/integrations/management/ManagementMenuEntityUpdater.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/management/ManagementMenuEntityUpdater.java
@@ -19,18 +19,26 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import fi.otavanopisto.kuntaapi.server.cache.MenuCache;
+import fi.otavanopisto.kuntaapi.server.cache.MenuItemCache;
 import fi.otavanopisto.kuntaapi.server.cache.ModificationHashCache;
 import fi.otavanopisto.kuntaapi.server.controllers.IdentifierController;
 import fi.otavanopisto.kuntaapi.server.discover.EntityUpdater;
 import fi.otavanopisto.kuntaapi.server.discover.OrganizationIdUpdateRequest;
+import fi.otavanopisto.kuntaapi.server.id.IdController;
+import fi.otavanopisto.kuntaapi.server.id.IdPair;
 import fi.otavanopisto.kuntaapi.server.id.MenuId;
 import fi.otavanopisto.kuntaapi.server.id.MenuItemId;
 import fi.otavanopisto.kuntaapi.server.id.OrganizationId;
+import fi.otavanopisto.kuntaapi.server.id.PageId;
+import fi.otavanopisto.kuntaapi.server.integrations.KuntaApiConsts;
+import fi.otavanopisto.kuntaapi.server.integrations.MenuProvider.MenuItemType;
 import fi.otavanopisto.kuntaapi.server.persistence.model.Identifier;
+import fi.otavanopisto.kuntaapi.server.rest.model.Menu;
+import fi.otavanopisto.kuntaapi.server.rest.model.MenuItem;
 import fi.otavanopisto.kuntaapi.server.settings.OrganizationSettingController;
 import fi.otavanopisto.kuntaapi.server.system.SystemUtils;
 import fi.otavanopisto.mwp.client.DefaultApi;
-import fi.otavanopisto.mwp.client.model.Menu;
 import fi.otavanopisto.mwp.client.model.Menuitem;
 
 @ApplicationScoped
@@ -55,6 +63,15 @@ public class ManagementMenuEntityUpdater extends EntityUpdater {
   
   @Inject
   private ModificationHashCache modificationHashCache;
+  
+  @Inject
+  private IdController idController;
+  
+  @Inject
+  private MenuCache menuCache;
+
+  @Inject
+  private MenuItemCache menuItemCache;
   
   @Resource
   private TimerService timerService;
@@ -121,21 +138,22 @@ public class ManagementMenuEntityUpdater extends EntityUpdater {
   }
 
   private void updateManagementMenus(OrganizationId organizationId) {
-    List<Menu> managementMenus = listManagementMenus(organizationId);
-    for (Menu managementMenu : managementMenus) {
-      updateManagementMenu(organizationId, managementMenu);
-    }
-    
-    List<Menuitem> managementMenuItems = listManagementMenuItems(organizationId, managementMenus);
-    for (Menuitem managementMenuItem : managementMenuItems) {
-      updateManagementMenuItem(organizationId, managementMenuItem);
+    List<fi.otavanopisto.mwp.client.model.Menu> managementMenus = listManagementMenus(organizationId);
+    for (fi.otavanopisto.mwp.client.model.Menu managementMenu : managementMenus) {
+      Menu menu = updateManagementMenu(organizationId, managementMenu);
+      MenuId menuId = new MenuId(organizationId, KuntaApiConsts.IDENTIFIER_NAME, menu.getId());
+      
+      List<Menuitem> managementMenuItems = listManagementMenuItems(organizationId, managementMenu);
+      for (Menuitem managementMenuItem : managementMenuItems) {
+        updateManagementMenuItem(organizationId, menuId, managementMenuItem);
+      }
     }
   }
 
-  private List<Menu> listManagementMenus(OrganizationId organizationId) {
+  private List<fi.otavanopisto.mwp.client.model.Menu> listManagementMenus(OrganizationId organizationId) {
     DefaultApi api = managementApi.getApi(organizationId);
     
-    fi.otavanopisto.mwp.client.ApiResponse<List<Menu>> response = api.kuntaApiMenusGet(null);
+    fi.otavanopisto.mwp.client.ApiResponse<List<fi.otavanopisto.mwp.client.model.Menu>> response = api.kuntaApiMenusGet(null);
     if (response.isOk()) {
       return response.getResponse();
     } else {
@@ -145,43 +163,136 @@ public class ManagementMenuEntityUpdater extends EntityUpdater {
     return Collections.emptyList();
   }
   
-  private List<Menuitem> listManagementMenuItems(OrganizationId organizationId, List<Menu> menus) {
+  private List<Menuitem> listManagementMenuItems(OrganizationId organizationId, fi.otavanopisto.mwp.client.model.Menu menu) {
     List<Menuitem> result = new ArrayList<>();
     
     DefaultApi api = managementApi.getApi(organizationId);
-    for (Menu menu : menus) {
-      String menuId = String.valueOf(menu.getId());
-      fi.otavanopisto.mwp.client.ApiResponse<List<Menuitem>> response = api.kuntaApiMenusMenuIdItemsGet(menuId);
-      if (response.isOk()) {
-        result.addAll(response.getResponse());
-      } else {
-        logger.warning(String.format("Listing menu %d items failed on [%d] %s", menu.getId(), response.getStatus(), response.getMessage()));
-      }
+    String menuId = String.valueOf(menu.getId());
+    fi.otavanopisto.mwp.client.ApiResponse<List<Menuitem>> response = api.kuntaApiMenusMenuIdItemsGet(menuId);
+    if (response.isOk()) {
+      result.addAll(response.getResponse());
+    } else {
+      logger.warning(String.format("Listing menu %d items failed on [%d] %s", menu.getId(), response.getStatus(), response.getMessage()));
     }
     
     return result;
   }
   
-  private void updateManagementMenu(OrganizationId organizationId, Menu managementMenu) {
-    MenuId menuId = new MenuId(organizationId, ManagementConsts.IDENTIFIER_NAME, String.valueOf(managementMenu.getId()));
+  private Menu updateManagementMenu(OrganizationId organizationId, fi.otavanopisto.mwp.client.model.Menu managementMenu) {
+    MenuId managementMenuId = new MenuId(organizationId, ManagementConsts.IDENTIFIER_NAME, String.valueOf(managementMenu.getId()));
 
-    Identifier identifier = identifierController.findIdentifierById(menuId);
+    Identifier identifier = identifierController.findIdentifierById(managementMenuId);
     if (identifier == null) {
-      identifier = identifierController.createIdentifier(menuId);
+      identifier = identifierController.createIdentifier(managementMenuId);
     }
     
-    modificationHashCache.put(identifier.getKuntaApiId(), createPojoHash(managementMenu));
+    MenuId kuntaApiMenuId = new MenuId(organizationId, KuntaApiConsts.IDENTIFIER_NAME, identifier.getKuntaApiId());
+    Menu menu = translateMenu(organizationId, managementMenu);
+    modificationHashCache.put(identifier.getKuntaApiId(), createPojoHash(menu));
+    menuCache.put(kuntaApiMenuId, menu);
+    
+    return menu;
   }
   
-  private void updateManagementMenuItem(OrganizationId organizationId, Menuitem managementMenuItem) {
-    MenuItemId menuItemId = new MenuItemId(organizationId, ManagementConsts.IDENTIFIER_NAME, String.valueOf(managementMenuItem.getId()));
+  private void updateManagementMenuItem(OrganizationId organizationId, MenuId menuId, Menuitem managementMenuItem) {
+    MenuItemId managementMenuItemId = new MenuItemId(organizationId, ManagementConsts.IDENTIFIER_NAME, String.valueOf(managementMenuItem.getId()));
 
-    Identifier identifier = identifierController.findIdentifierById(menuItemId);
+    Identifier identifier = identifierController.findIdentifierById(managementMenuItemId);
     if (identifier == null) {
-      identifier = identifierController.createIdentifier(menuItemId);
+      identifier = identifierController.createIdentifier(managementMenuItemId);
     }
     
-    modificationHashCache.put(identifier.getKuntaApiId(), createPojoHash(managementMenuItem));
+    MenuItem menuItem = translateMenuItem(organizationId, managementMenuItem);
+    MenuItemId kuntaApiMenuItemId = new MenuItemId(organizationId, KuntaApiConsts.IDENTIFIER_NAME, identifier.getKuntaApiId());
+        
+    modificationHashCache.put(identifier.getKuntaApiId(), createPojoHash(menuItem));
+    menuItemCache.put(new IdPair<MenuId, MenuItemId>(menuId,kuntaApiMenuItemId), menuItem);
   }
 
+  private Menu translateMenu(OrganizationId organizationId, fi.otavanopisto.mwp.client.model.Menu managementMenu) {
+    Menu menu = new Menu();
+    
+    MenuId managementMenuId = new MenuId(organizationId, ManagementConsts.IDENTIFIER_NAME, String.valueOf(managementMenu.getId()));
+    MenuId kuntaApiMenuId = idController.translateMenuId(managementMenuId, KuntaApiConsts.IDENTIFIER_NAME);
+    if (kuntaApiMenuId == null) {
+      logger.info(String.format("Could not translate management menu %d into kunta api id", managementMenu.getId()));
+      return null;
+    }
+    
+    menu.setId(kuntaApiMenuId.getId());
+    menu.setSlug(managementMenu.getSlug());
+    
+    return menu;
+  }
+  
+  private MenuItem translateMenuItem(OrganizationId organizationId, fi.otavanopisto.mwp.client.model.Menuitem managementMenuItem) {
+    MenuItem menuItem = new MenuItem();
+    
+    MenuItemId managementMenuItemId = new MenuItemId(organizationId,ManagementConsts.IDENTIFIER_NAME, String.valueOf(managementMenuItem.getId()));
+    MenuItemId kuntaApiMenuItemId = idController.translateMenuItemId(managementMenuItemId, KuntaApiConsts.IDENTIFIER_NAME);
+    if (kuntaApiMenuItemId == null) {
+      logger.info(String.format("Could not translate management menu item %d into kunta api id", managementMenuItem.getId()));
+      return null;
+    }
+    
+    MenuItemType itemType = getItemType(managementMenuItem);
+    if (itemType == null) {
+      logger.severe(String.format("Could not determine item type for %d", managementMenuItem.getId()));
+      return null;
+    }
+    
+    PageId pageId = translatePageId(organizationId, managementMenuItem.getPageId());
+    MenuItemId parentMenuItemId = translateMenuItemId(organizationId, managementMenuItem.getParentItemId());
+    
+    menuItem.setId(kuntaApiMenuItemId.getId());
+    menuItem.setLabel(managementMenuItem.getTitle());
+    menuItem.setFileId(null);
+    menuItem.setExternalUrl(itemType == MenuItemType.LINK ? managementMenuItem.getUrl() : null);
+    menuItem.setPageId(pageId != null ? pageId.getId() : null);
+    menuItem.setParentItemId(parentMenuItemId != null ? parentMenuItemId.getId() : null);
+    menuItem.setType(itemType.toString());
+    
+    return menuItem;
+  }
+
+  private MenuItemId translateMenuItemId(OrganizationId organizationId, Long parentItemId) {
+    if (parentItemId == null) {
+      return null;
+    }
+    
+    MenuItemId managementMenuItem = new MenuItemId(organizationId, ManagementConsts.IDENTIFIER_NAME, String.valueOf(parentItemId));
+
+    return idController.translateMenuItemId(managementMenuItem, KuntaApiConsts.IDENTIFIER_NAME);
+  }
+
+  private MenuItemType getItemType(fi.otavanopisto.mwp.client.model.Menuitem managementMenuItem) {
+    switch (managementMenuItem.getType()) {
+      case "page":
+        return MenuItemType.PAGE;
+      case "post":
+        return MenuItemType.NEWS_ARTICLE;
+      case "custom":
+        return MenuItemType.LINK;
+      default:
+        return null;
+    }
+  }
+  
+  private PageId translatePageId(OrganizationId organizationId, Long pageId) {
+    if (pageId == null) {
+      return null;
+    }
+    
+    return translatePageId(organizationId, pageId.intValue());
+  }
+  
+  private PageId translatePageId(OrganizationId organizationId, Integer pageId) {
+    if (pageId == null) {
+      return null;
+    }
+    
+    PageId managementId = new PageId(organizationId, ManagementConsts.IDENTIFIER_NAME, String.valueOf(pageId));
+    return idController.translatePageId(managementId, KuntaApiConsts.IDENTIFIER_NAME);
+  }
+  
 }
