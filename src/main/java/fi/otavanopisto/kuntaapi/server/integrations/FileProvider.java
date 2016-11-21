d9cee46d70ead10ac9fea48edb89f25112ef34d8
Antti Leppä <antti.leppa@metatavu.fi>
2016-11-21 10:10:36 +0200

    Added FileProvider interface

---
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/FileProvider.java
@@ -0,0 +1,44 @@
+package fi.otavanopisto.kuntaapi.server.integrations;
+
+import java.util.List;
+
+import fi.otavanopisto.kuntaapi.server.id.FileId;
+import fi.otavanopisto.kuntaapi.server.id.OrganizationId;
+import fi.otavanopisto.kuntaapi.server.id.PageId;
+import fi.otavanopisto.kuntaapi.server.rest.model.FileDef;
+
+/**
+ * Interface that describes a single file provider
+ * 
+ * @author Antti Leppä
+ */
+public interface FileProvider {
+
+  /**
+   * List files in an organization
+   * 
+   * @param organizationId organization id
+   * @param filter results by page id (optional)
+   * @return list of organization files
+   */
+  public List<FileDef> listOrganizationFiles(OrganizationId organizationId, PageId pageId);
+  
+  /**
+   * Finds a single organization file
+   * 
+   * @param organizationId organization id
+   * @param fileId file id
+   * @return file or null of not found
+   */
+  public FileDef findOrganizationFile(OrganizationId organizationId, FileId fileId);
+  
+  /**
+   * Returns organization file data
+   * 
+   * @param organizationId organization id
+   * @param fileId file id
+   * @return organization file data
+   */
+  public AttachmentData getOrganizationFileData(OrganizationId organizationId, FileId fileId);
+
+}
