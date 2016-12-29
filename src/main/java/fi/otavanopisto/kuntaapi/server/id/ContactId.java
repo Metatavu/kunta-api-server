db4eeebb2e7bc2066dce973b480c317f0cb28ec9
Antti Leppä <antti.leppa@metatavu.fi>
2016-12-29 08:38:43 +0200

    Added contact id

---
+++ src/main/java/fi/otavanopisto/kuntaapi/server/id/ContactId.java
@@ -0,0 +1,44 @@
+package fi.otavanopisto.kuntaapi.server.id;
+
+/**
+ * Class representing contact id
+ * 
+ * @author Antti Leppä
+ */
+public class ContactId extends OrganizationBaseId {
+
+  private static final long serialVersionUID = 6959970320549914044L;
+
+  /**
+   * Zero-argument constructor for contact id
+   */
+  public ContactId() {
+    super();
+  }
+
+  /**
+   * Constructor that accepts source and id
+   * 
+   * @param source source
+   * @param id id
+   */
+  public ContactId(OrganizationId organizationId, String source, String id) {
+    super(organizationId, source, id);
+  }
+  
+  @Override
+  public IdType getType() {
+    return IdType.CONTACT;
+  }
+  
+  @Override
+  protected int getHashInitial() {
+    return 333;
+  }
+  
+  @Override
+  protected int getHashMultiplier() {
+    return 345;
+  }
+  
+}
--- src/main/java/fi/otavanopisto/kuntaapi/server/id/IdType.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/id/IdType.java
@@ -43,5 +43,7 @@ public enum IdType {
 
   JOB,
 
-  ANNOUNCEMENT
+  ANNOUNCEMENT,
+  
+  CONTACT
 }
