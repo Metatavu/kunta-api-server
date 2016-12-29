3160c4415fcb4902e407f5210d537547d0a01836
Antti Leppä <antti.leppa@metatavu.fi>
2016-12-29 11:24:33 +0200

    Initial version of VCardEntityUpdater

---
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/vcard/VCardConsts.java
@@ -0,0 +1,20 @@
+package fi.otavanopisto.kuntaapi.server.integrations.vcard;
+
+/**
+ * Constants for VCard integration
+ * 
+ * @author Antti Leppä
+ */
+@SuppressWarnings ("squid:S2068")
+public class VCardConsts {
+  
+  public static final String IDENTIFIER_NAME = "vcard";
+  public static final String ORGANIZATION_SETTING_URL = "vcard.url";
+  public static final String ORGANIZATION_SETTING_USERNAME = "vcard.username";
+  public static final String ORGANIZATION_SETTING_PASSWORD = "vcard.password";
+  public static final String DEFAULT_LANGUAGE = "fi";
+  
+  private VCardConsts() {
+  }
+
+}
