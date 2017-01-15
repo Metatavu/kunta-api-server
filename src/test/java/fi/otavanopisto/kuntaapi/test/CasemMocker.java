08fa1040c825ca0425cdbafea18deb335efd2e2c
Antti Leppä <antti.leppa@metatavu.fi>
2017-01-15 09:06:49 +0200

    Sonar issues

--- src/test/java/fi/otavanopisto/kuntaapi/test/CasemMocker.java
+++ src/test/java/fi/otavanopisto/kuntaapi/test/CasemMocker.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class CasemMocker extends AbstractMocker {
   
+  private static final String APPLICATION_JSON = "application/json";
   private static final String URL_PREFIX = "/casem/api/opennc/v1/";
   
   public CasemMocker mockSubnodes(Integer rootNode, Integer...subNodes) {
@@ -26,21 +27,21 @@ public class CasemMocker extends AbstractMocker {
     
     String file = String.format("subnodes/%d.json", subNodes == null || subNodes.length == 0 ? rootNode : subNodes[subNodes.length - 1]);
     
-    mockGetString(String.format("%s/%s", URL_PREFIX, path), "application/json", readCasemFile(file));
+    mockGetString(String.format("%s/%s", URL_PREFIX, path), APPLICATION_JSON, readCasemFile(file));
     
     return this;
   }
   
   public CasemMocker mockContentList() {
-    mockGetString(String.format("%s/Contents()", URL_PREFIX), "application/json", readCasemFile("contents.json"));
-    mockGetString(String.format("%s/Contents", URL_PREFIX), "application/json", readCasemFile("contents.json"));
+    mockGetString(String.format("%s/Contents()", URL_PREFIX), APPLICATION_JSON, readCasemFile("contents.json"));
+    mockGetString(String.format("%s/Contents", URL_PREFIX), APPLICATION_JSON, readCasemFile("contents.json"));
     return this;
   }
   
   public CasemMocker mockContent(Integer... contentIds) {
     for (Integer contentId : contentIds) {
-      mockGetString(String.format("%s/Contents(%d)", URL_PREFIX, contentId), "application/json", readCasemFile(String.format("contents/%d.json", contentId)));
-      mockGetString(String.format("%s/Contents(%d)/ExtendedProperties", URL_PREFIX, contentId), "application/json", readCasemFile(String.format("contents/%d_ext.json", contentId)));
+      mockGetString(String.format("%s/Contents(%d)", URL_PREFIX, contentId), APPLICATION_JSON, readCasemFile(String.format("contents/%d.json", contentId)));
+      mockGetString(String.format("%s/Contents(%d)/ExtendedProperties", URL_PREFIX, contentId), APPLICATION_JSON, readCasemFile(String.format("contents/%d_ext.json", contentId)));
     }
     
     return this;
