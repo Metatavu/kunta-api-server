c7143f6a3834dca109fbaf052ab99d8ca44766fa
Antti Leppä <antti.leppa@metatavu.fi>
2017-02-10 18:57:29 +0200

    Changed management provider to use identifier based listing

--- src/main/java/fi/otavanopisto/kuntaapi/server/integrations/management/ManagementNewsProvider.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/management/ManagementNewsProvider.java
@@ -8,17 +8,18 @@ import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 
+import fi.metatavu.kuntaapi.server.rest.model.Attachment;
+import fi.metatavu.kuntaapi.server.rest.model.NewsArticle;
+import fi.metatavu.management.client.model.Attachment.MediaTypeEnum;
 import fi.otavanopisto.kuntaapi.server.cache.NewsArticleCache;
 import fi.otavanopisto.kuntaapi.server.cache.NewsArticleImageCache;
+import fi.otavanopisto.kuntaapi.server.controllers.IdentifierController;
 import fi.otavanopisto.kuntaapi.server.id.AttachmentId;
 import fi.otavanopisto.kuntaapi.server.id.IdPair;
 import fi.otavanopisto.kuntaapi.server.id.NewsArticleId;
 import fi.otavanopisto.kuntaapi.server.id.OrganizationId;
 import fi.otavanopisto.kuntaapi.server.integrations.AttachmentData;
 import fi.otavanopisto.kuntaapi.server.integrations.NewsProvider;
-import fi.metatavu.kuntaapi.server.rest.model.Attachment;
-import fi.metatavu.kuntaapi.server.rest.model.NewsArticle;
-import fi.metatavu.management.client.model.Attachment.MediaTypeEnum;
 
 /**
  * News provider for management wordpress
@@ -27,6 +28,9 @@ import fi.metatavu.management.client.model.Attachment.MediaTypeEnum;
  */
 @RequestScoped
 public class ManagementNewsProvider extends AbstractManagementProvider implements NewsProvider {
+  
+  @Inject
+  private IdentifierController identifierController;
   
   @Inject
   private ManagementImageLoader managementImageLoader;
@@ -43,8 +47,7 @@ public class ManagementNewsProvider extends AbstractManagementProvider implement
       return Collections.emptyList();
     }
     
-    List<NewsArticleId> newsArticleIds = newsArticleCache.getOragnizationIds(organizationId);
-    
+    List<NewsArticleId> newsArticleIds = identifierController.listOrganizationNewsArticleIdsBySource(organizationId, ManagementConsts.IDENTIFIER_NAME);
     List<NewsArticle> result = new ArrayList<>(newsArticleIds.size());
     for (NewsArticleId newsArticleId : newsArticleIds) {
       NewsArticle newsArticle = newsArticleCache.get(newsArticleId);
@@ -65,9 +68,9 @@ public class ManagementNewsProvider extends AbstractManagementProvider implement
   public List<Attachment> listNewsArticleImages(OrganizationId organizationId, NewsArticleId newsArticleId) {
     List<Attachment> result = new ArrayList<>();
     
-    List<IdPair<NewsArticleId,AttachmentId>> childIds = newsArticleImageCache.getChildIds(newsArticleId);
-    for (IdPair<NewsArticleId,AttachmentId> childId : childIds) {
-      Attachment attachment = newsArticleImageCache.get(childId);
+    List<AttachmentId> attachmentIds = identifierController.listAttachmentIdsBySourceAndParentId(ManagementConsts.IDENTIFIER_NAME, newsArticleId);
+    for (AttachmentId attachmentId : attachmentIds) {
+      Attachment attachment = newsArticleImageCache.get(new IdPair<NewsArticleId, AttachmentId>(newsArticleId, attachmentId));
       if (attachment != null) {
         result.add(attachment);
       }
