2c3ed17ae36fced47840f32838bf663fee860e02
Antti Leppä <antti.leppa@metatavu.fi>
2017-02-23 12:49:08 +0200

    Fixed kunta rekry task queue scope

--- src/main/java/fi/otavanopisto/kuntaapi/server/integrations/kuntarekry/tasks/KuntaRekryJobTaskQueue.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/kuntarekry/tasks/KuntaRekryJobTaskQueue.java
@@ -1,8 +1,11 @@
 package fi.otavanopisto.kuntaapi.server.integrations.kuntarekry.tasks;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import fi.otavanopisto.kuntaapi.server.integrations.kuntarekry.KuntaRekryConsts;
 import fi.otavanopisto.kuntaapi.server.tasks.AbstractTaskQueue;
 
+@ApplicationScoped
 public class KuntaRekryJobTaskQueue extends AbstractTaskQueue<KuntaRekryJobEntityTask> {
 
   @Override
