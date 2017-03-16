03f4b922ec425790ee8af0183411dd569d9ccac3
Antti Leppä <antti.leppa@metatavu.fi>
2017-03-16 06:07:59 +0200

    Added an entity for queued tasks

--- src/main/java/fi/otavanopisto/kuntaapi/server/persistence/dao/AbstractDAO.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/persistence/dao/AbstractDAO.java
@@ -11,6 +11,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
+import org.hibernate.jpa.criteria.compile.CriteriaQueryTypeQueryAdapter;
+
 /**
  * Abstract base class for all DAO classes
  * 
@@ -128,6 +130,16 @@ public abstract class AbstractDAO<T> {
     }
 
     return list.get(list.size() - 1);
+  }
+  
+  /**
+   * Prints query as HQL. Used for debugging purposes only
+   * 
+   * @param query query
+   * @return query as HQL
+   */
+  protected String getQueryHQL(Query query) {
+    return ((CriteriaQueryTypeQueryAdapter<?>) query).getHibernateQuery().getQueryString();
   }
 
   private Class<?> getFirstTypeArgument(ParameterizedType parameterizedType) {
---
+++ src/main/java/fi/otavanopisto/kuntaapi/server/persistence/dao/TaskDAO.java
@@ -0,0 +1,48 @@
+package fi.otavanopisto.kuntaapi.server.persistence.dao;
+
+import java.time.OffsetDateTime;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import fi.otavanopisto.kuntaapi.server.persistence.model.Task;
+import fi.otavanopisto.kuntaapi.server.persistence.model.Task_;
+
+/**
+ * DAO class for Task entity
+ * 
+ * @author Antti Leppä
+ */
+@ApplicationScoped
+public class TaskDAO extends AbstractDAO<Task> {
+  
+  public Task create(String queue, Boolean priority, byte[] data, OffsetDateTime created) {
+    Task task = new Task();
+    task.setCreated(created);
+    task.setData(data);
+    task.setPriority(priority);
+    task.setQueue(queue);
+    
+    return persist(task);
+  }
+  
+  public Task findByNextInQueue(String queue) {
+    EntityManager entityManager = getEntityManager();
+
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Task> criteria = criteriaBuilder.createQuery(Task.class);
+    Root<Task> root = criteria.from(Task.class);
+    criteria.select(root);
+    criteria.where(criteriaBuilder.equal(root.get(Task_.queue), queue));
+    criteria.orderBy(criteriaBuilder.desc(root.get(Task_.priority)), criteriaBuilder.asc(root.get(Task_.created)));
+    TypedQuery<Task> query = entityManager.createQuery(criteria);
+    query.setMaxResults(1);
+    
+    return getSingleResult(query);
+  }
+  
+}
