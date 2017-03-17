72d691c7ee140a38e61ab8a98d0043f6f8ccabc7
Antti Leppä <antti.leppa@metatavu.fi>
2017-03-17 10:41:07 +0200

    Added a ClusterController

---
+++ src/main/java/fi/otavanopisto/kuntaapi/server/controllers/ClusterController.java
@@ -0,0 +1,41 @@
+package fi.otavanopisto.kuntaapi.server.controllers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.wildfly.clustering.group.Group;
+import org.wildfly.clustering.group.Node;
+
+@ApplicationScoped
+public class ClusterController {
+
+  @Resource(lookup = "java:jboss/clustering/group/web")  
+  private Group channelGroup;  
+
+  public String getLocalNodeName() {
+    Node localNode = channelGroup.getLocalNode();
+    if (localNode != null) {
+      return localNode.getName();
+    }
+    
+    return "UNKNOWN";
+  }
+  
+  public List<String> getNodeNames() {
+    List<Node> nodes = channelGroup.getNodes();
+    
+    List<String> result = new ArrayList<>(nodes.size());
+    for (Node node : nodes) {
+      result.add(node.getName());
+    }
+    
+    Collections.sort(result);
+    
+    return result;
+  }
+  
+}
