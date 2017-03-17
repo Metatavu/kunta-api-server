e7fd713e30b9f6adf108c97c215f53f987e3b854
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-03-17 12:04:26 +0200

    Removed unnecessary code

--- src/main/java/fi/otavanopisto/kuntaapi/server/controllers/ClusterController.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/controllers/ClusterController.java
@@ -3,40 +3,22 @@ package fi.otavanopisto.kuntaapi.server.controllers;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.annotation.PostConstruct;
 
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import org.wildfly.clustering.group.Group;
 import org.wildfly.clustering.group.Node;
 
 @ApplicationScoped
+@Singleton
 public class ClusterController {
 
-  @Inject
-  private Logger logger;
-  
-  private Group channelGroup;
-  
-  @PostConstruct
-  private void postConstruct() {
-    try {  
-      channelGroup = (Group) new InitialContext().lookup("java:jboss/clustering/group/web");
-    } catch (NamingException ex) {
-      logger.log(Level.SEVERE, "Cannot find java:jboss/clustering/group/web disabling task distribution");
-    }
-  }
+  @Resource(lookup = "java:jboss/clustering/group/web")  
+  private Group channelGroup;  
 
   public String getLocalNodeName() {
-    if (channelGroup == null) {
-      return "UNKNOWN";
-    }
-    
     Node localNode = channelGroup.getLocalNode();
     if (localNode != null) {
       return localNode.getName();
@@ -46,10 +28,6 @@ public class ClusterController {
   }
   
   public List<String> getNodeNames() {
-    if (channelGroup == null) {
-      return Collections.singletonList("UNKNOWN");
-    }
-    
     List<Node> nodes = channelGroup.getNodes();
     
     List<String> result = new ArrayList<>(nodes.size());
