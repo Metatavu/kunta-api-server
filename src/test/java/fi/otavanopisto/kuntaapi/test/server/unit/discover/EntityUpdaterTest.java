d565ca386a63bde3699778f94e295deb8caeed76
Antti Leppä <antti.leppa@metatavu.fi>
2017-03-18 14:43:18 +0200

    Fixed correct annotation to updater tests

--- src/test/java/fi/otavanopisto/kuntaapi/test/server/unit/discover/EntityUpdaterTest.java
+++ src/test/java/fi/otavanopisto/kuntaapi/test/server/unit/discover/EntityUpdaterTest.java
@@ -6,8 +6,8 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.util.Iterator;
 
+import javax.ejb.AccessTimeout;
 import javax.ejb.Singleton;
-import javax.ejb.Timeout;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
@@ -42,7 +42,7 @@ public class EntityUpdaterTest {
       
       assertTrue(String.format("Singleton not present in %s", entityUpdaterClass.getName()), entityUpdaterClass.isAnnotationPresent(Singleton.class));
       assertTrue(String.format("ApplicationScoped not present in %s", entityUpdaterClass.getName()), entityUpdaterClass.isAnnotationPresent(ApplicationScoped.class));
-      assertTrue(String.format("Timeout not present in %s", entityUpdaterClass.getName()), entityUpdaterClass.isAnnotationPresent(Timeout.class));
+      assertTrue(String.format("AccessTimeout not present in %s", entityUpdaterClass.getName()), entityUpdaterClass.isAnnotationPresent(AccessTimeout.class));
     }
   }
 
