54c8de0028ee33d01b65c873f518911bfcdaa413
Antti Leppä <antti.leppa@metatavu.fi>
2017-03-22 10:27:13 +0200

    Added tests for service's channel id list and service channel redirects

--- src/test/java/fi/otavanopisto/kuntaapi/test/server/integration/restfulptv/ServicesTestsIT.java
+++ src/test/java/fi/otavanopisto/kuntaapi/test/server/integration/restfulptv/ServicesTestsIT.java
@@ -1,6 +1,8 @@
 package fi.otavanopisto.kuntaapi.test.server.integration.restfulptv;
 
 import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.config.RedirectConfig.redirectConfig;
+import static com.jayway.restassured.config.RestAssuredConfig.newConfig;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.hamcrest.core.IsNull.nullValue;
@@ -40,6 +42,21 @@ public class ServicesTestsIT extends AbstractIntegrationTest {
     
     getRestfulPtvServiceMocker()
       .mockServices("6c9926b9-4aa0-4635-b66a-471af07dfec3", "822d5347-8398-4866-bb9d-9cdc60b38fba", "ef66b7c2-e938-4a30-ad57-475fc40abf27");
+    
+    getRestfulPtvElectronicServiceChannelMocker()
+      .mockElectronicServiceChannels("22472ece-95a0-4fef-a429-b4da689677b2", "44187ff9-71ed-40df-89f6-916be4f3baa6", "799e0e4f-4da7-4e7d-9e0e-f1370b80fc9a");
+    
+    getRestfulPtvPhoneServiceChannelMocker()
+      .mockPhoneServiceChannels("108f0c61-bfba-4dd7-8f02-deb4e77c52d0", "626cdd7a-e205-42da-8ce5-82b3b7add258", "e9e86a9e-6593-469d-bc01-f1a59c28168d");
+      
+    getRestfulPtvPrintableFormServiceChannelMocker()
+      .mockPrintableFormServiceChannels("02256ce8-2879-47e4-a6f5-339872f0f758", "1a17f994-b924-46ae-8708-c09938125119", "6fb56241-1b43-4e42-8231-43ba8d86be36");
+      
+    getRestfulPtvServiceLocationServiceChannelMocker()
+      .mockServiceLocationServiceChannels("9a9f5def-92e4-4b79-a49a-ccf20a0f75b6", "c0681f51-d1b4-4a9b-bbbf-ddf9a5273cd1", "cf927001-8b45-4f08-b93b-c78fe8477928");
+      
+    getRestfulPtvWebPageServiceChannelMocker()
+      .mockWebPageServiceChannels("4b08ae17-75ae-4746-9382-1316c4ec02c5", "aedae320-a2b2-4fe6-b23b-2e1a025ba415", "e9ec256b-5ca2-4663-9da6-d8a2faff21a8");
     
     startMocks();
      
@@ -198,5 +215,65 @@ public class ServicesTestsIT extends AbstractIntegrationTest {
     assertEquals(serviceId, getServiceId(2));
   }
   
+  @Test
+  public void testServiceChannelIds()  {
+    String id = given() 
+      .baseUri(getApiBasePath())
+      .contentType(ContentType.JSON)
+      .get("/services")
+      .body().jsonPath().getString("id[0]");
+      
+    assertNotNull(id);
+      
+    given() 
+      .baseUri(getApiBasePath())
+      .contentType(ContentType.JSON)
+      .get("/services/{serviceId}", id)
+      .then()
+      .assertThat()
+      .statusCode(200)
+      .body("id", notNullValue())
+      .body("electronicServiceChannelIds.size()", is(1))
+      .body("phoneServiceChannelIds.size()", is(2))
+      .body("printableFormServiceChannelIds.size()", is(1))
+      .body("serviceLocationServiceChannelIds.size()", is(2))
+      .body("webPageServiceChannelIds.size()", is(1));
+  }
+  
+  @Test
+  public void testServiceChannelRedirects() {
+    testServiceChannelRedirect("electronicChannels", "electronicServiceChannels");
+    testServiceChannelRedirect("phoneChannels", "phoneServiceChannels");
+    testServiceChannelRedirect("printableFormChannels", "printableFormServiceChannels");
+    testServiceChannelRedirect("serviceLocationChannels", "serviceLocationServiceChannels");
+    testServiceChannelRedirect("webPageChannels", "webPageServiceChannels");
+  }
+  
+  private void testServiceChannelRedirect(String from, String to) {
+    String fromListPath = String.format("/services/SERVICE_ID/%s", from);
+    String fromFindPath = String.format("%sCHANNEL_ID", fromListPath);
+    String toListPath = String.format("/%s", to);
+    String toFindPath = String.format("%s%s", toListPath, "SERVICE_ID");
+
+    given() 
+      .baseUri(getApiBasePath())
+      .contentType(ContentType.JSON)
+      .config(newConfig().redirect(redirectConfig().followRedirects(false)))
+      .get(fromListPath)
+      .then()
+      .assertThat()
+      .statusCode(307)
+      .header("Location", String.format("%s%s", getApiBasePath(), toListPath));
+    
+    given() 
+      .baseUri(getApiBasePath())
+      .contentType(ContentType.JSON)
+      .config(newConfig().redirect(redirectConfig().followRedirects(false)))
+      .get(fromFindPath)
+      .then()
+      .assertThat()
+      .statusCode(307)
+      .header("Location", String.format("%s%s", getApiBasePath(), toFindPath));
+  }
   
 }
