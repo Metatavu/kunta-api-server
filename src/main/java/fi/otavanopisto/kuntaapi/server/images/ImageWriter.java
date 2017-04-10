f0d5901a09ea01ec79830f4aa544887537b26b6f
Antti Leppä <antti.leppa@metatavu.fi>
2017-04-10 07:04:26 +0300

    Changed image scalers to prefer original image format

--- src/main/java/fi/otavanopisto/kuntaapi/server/images/ImageWriter.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/images/ImageWriter.java
@@ -6,9 +6,11 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.ApplicationScoped;
 import javax.imageio.ImageIO;
 import javax.inject.Inject;
+
+import org.apache.commons.codec.binary.StringUtils;
 
 /**
  * Image writer
@@ -16,33 +18,59 @@ import javax.inject.Inject;
  * @author Antti Leppä
  * @author Heikki Kurhinen
  */
-@Dependent
+@ApplicationScoped
 public class ImageWriter {
  
   @Inject
   private Logger logger;
+  
+  /**
+   * Returns preferred ImageIO format for given content type
+   * 
+   * @param contentType content type
+   * @return preferred ImageIO format for given content type
+   */
+  public String getFormatName(String contentType) {
+    if (StringUtils.equals(contentType, "image/png")) {
+      return "png";
+    } 
 
-  private ImageWriter() {
+    return "jpg";
   }
   
   /**
-   * Writes buffered image as PNG byte array
+   * Returns appropriate content type for given ImageIO format
+   * 
+   * @param formatName format name
+   * @return appropriate content type for given ImageIO format
+   */
+  public String getContentTypeForFormatName(String formatName) {
+    if (StringUtils.equals(formatName, "png")) {
+      return "image/png";
+    } 
+
+    return "image/jpeg";
+  }
+  
+  /**
+   * Writes buffered image as byte array
    * 
    * @param image image
+   * @param formatName target format name
    * @return image data as byte array or null when writing has failed
    */
   @SuppressWarnings ("squid:S1168")
-  public byte[] writeBufferedImageAsPng(BufferedImage image) {
+  public byte[] writeBufferedImage(BufferedImage image, String formatName) {
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     
     try {
-      if (ImageIO.write(image, "png", outputStream)) {
+      if (ImageIO.write(image, formatName, outputStream)) {
         outputStream.flush();
         outputStream.close();
         return outputStream.toByteArray();
       }
     } catch (IOException e) {
-      logger.log(Level.SEVERE, "Failed to write buffered image as png", e);
+      logger.log(Level.SEVERE, "Failed to write buffered image", e);
     }
     
     return null;
