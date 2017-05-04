99331bd0534f6a8f84162da1a28758cc7f2ec66e
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-05-04 14:02:03 +0300

    Sonar issues

--- src/main/java/fi/otavanopisto/kuntaapi/server/integrations/ptv/PtvConsts.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/ptv/PtvConsts.java
@@ -1,5 +1,9 @@
 package fi.otavanopisto.kuntaapi.server.integrations.ptv;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Constants for palvelutietoväylä
  * 
@@ -13,7 +17,7 @@ public class PtvConsts {
   public static final String SYSTEM_SETTING_BASEURL = "ptv.baseUrl";
   public static final String DEFAULT_LANGUAGE = "fi";
   public static final String TIMEZONE = "Europe/Helsinki";
-  public static final String[] PTV_SUPPORTED_LANGUAGES = {"fi", "en", "sv"};
+  public static final List<String> PTV_SUPPORTED_LANGUAGES = Collections.unmodifiableList(Arrays.asList("fi", "en", "sv"));
   
   private PtvConsts() {
   }
