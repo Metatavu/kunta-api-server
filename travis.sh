923cd5c6419336f94aa9d0226ec8a2747f8ea337
Antti Leppä <antti.leppa@metatavu.fi>
2017-09-05 05:54:35 +0300

    Travis

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ $TRAVIS_BRANCH != "master" ] && [ -n "${GITHUB_TOKEN}" ] && [ -n "${SONAR_TOKEN}" ]; then
   echo "Pull request"
   
-  PROJECT_VERSION=`cat pom.xml|grep version -m 1|sed -e \'s/.*<version>//\'|sed -e \'s/<.*//\'`
+  PROJECT_VERSION=`cat pom.xml|grep version -m 1|sed -e 's/.*<version>//'|sed -e 's/<.*//'`
   
   sh sonar-scanner/bin/sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL \
     -Dsonar.analysis.mode=issues \
