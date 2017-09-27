8bb6292ff2508b25067bec63f0bdb19e0e730bf8
Antti Leppä <antti.leppa@metatavu.fi>
2017-09-27 11:33:48 +0300

    Fixed issue #376

--- src/main/java/fi/otavanopisto/kuntaapi/server/integrations/linkedevents/LinkedEventsConsts.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/linkedevents/LinkedEventsConsts.java
@@ -10,6 +10,7 @@ public class LinkedEventsConsts {
   public static final String IDENTIFIER_NAME = "linkedevents";
   public static final String ORGANIZATION_SETTING_BASEURL = "linkedevents.baseUrl";
   public static final String ORGANIZATION_SETTING_ORGANIZATION = "linkedevents.organization";
+  public static final String ATTACHMENT_TYPE_EVENT_IMAGE = "event-image";
   
   private LinkedEventsConsts() {
   }
