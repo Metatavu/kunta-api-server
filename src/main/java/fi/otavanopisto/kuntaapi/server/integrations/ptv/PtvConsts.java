430741cf8caaa0ba9eace40fdceca6de16432b3b
Antti Leppä <antti.leppa@metatavu.fi>
2017-10-22 14:31:05 +0300

    Added PTV access token updater

--- src/main/java/fi/otavanopisto/kuntaapi/server/integrations/ptv/PtvConsts.java
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/ptv/PtvConsts.java
@@ -16,9 +16,13 @@ public class PtvConsts {
   public static final boolean SYNCHRONIZE = true;
   public static final String CACHE_NAME = "ptv";
   public static final String SYSTEM_SETTING_BASEURL = "ptv.baseUrl";
+  public static final String SYSTEM_SETTING_STS_BASEURL = "ptv.stsBaseUrl";
   public static final String DEFAULT_LANGUAGE = "fi";
   public static final String TIMEZONE = "Europe/Helsinki";
   public static final List<String> PTV_SUPPORTED_LANGUAGES = Collections.unmodifiableList(Arrays.asList("fi", "en", "sv"));
+  public static final String ORGANIZATION_SETTING_API_USER = "ptv.apiuser";
+  public static final String ORGANIZATION_SETTING_API_PASS = "ptv.apipass";
+  public static final String PTV_ACCESS_TOKEN_TYPE = "ptv";
   
   private PtvConsts() {
   }
---
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/ptv/tasks/AccessTokenTaskQueue.java
@@ -0,0 +1,21 @@
+package fi.otavanopisto.kuntaapi.server.integrations.ptv.tasks;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import fi.otavanopisto.kuntaapi.server.integrations.ptv.PtvConsts;
+import fi.otavanopisto.kuntaapi.server.tasks.AbstractOrganizationEntityTaskQueue;
+
+@ApplicationScoped
+public class AccessTokenTaskQueue extends AbstractOrganizationEntityTaskQueue {
+
+  @Override
+  public String getSource() {
+    return  PtvConsts.IDENTIFIER_NAME;
+  }
+
+  @Override
+  public String getEntity() {
+    return "access-token";
+  }
+  
+}
