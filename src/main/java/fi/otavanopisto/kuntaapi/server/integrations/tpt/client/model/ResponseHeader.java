595623e8f462308ac3516de55788e51ce2631942
Antti Leppä <antti.leppa@metatavu.fi>
2018-04-17 07:35:13 +0300

    Added model for tpt api

---
+++ src/main/java/fi/otavanopisto/kuntaapi/server/integrations/tpt/client/model/ResponseHeader.java
@@ -0,0 +1,55 @@
+package fi.otavanopisto.kuntaapi.server.integrations.tpt.client.model;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ResponseHeader implements Serializable {
+
+  private static final long serialVersionUID = 6432250874166877639L;
+
+  @JsonProperty("qtime")
+  private Integer qtime;
+
+  @JsonProperty("QTime")
+  private Integer QTime2;
+
+  @JsonProperty("params")
+  private Params params;
+
+  @JsonProperty("status")
+  private Integer status;
+
+  public Integer getQtime() {
+    return qtime;
+  }
+
+  public void setQtime(Integer qtime) {
+    this.qtime = qtime;
+  }
+  
+  public Integer getQTime2() {
+    return QTime2;
+  }
+  
+  public void setQTime2(Integer qTime2) {
+    QTime2 = qTime2;
+  }
+  
+  public Params getParams() {
+    return params;
+  }
+
+  public void setParams(Params params) {
+    this.params = params;
+  }
+
+  public Integer getStatus() {
+    return status;
+  }
+
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
+
+}
