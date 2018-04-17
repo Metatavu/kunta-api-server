61ac0bb57262b99938f1e973a9e84d36ae019cf7
Antti Leppä <antti.leppa@metatavu.fi>
2018-04-17 07:36:48 +0300

    Added unit tests for tpt translator

---
+++ src/test/java/fi/otavanopisto/kuntaapi/test/server/unit/tpt/TptTranslatorTest.java
@@ -0,0 +1,102 @@
+package fi.otavanopisto.kuntaapi.test.server.unit.tpt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.ZoneId;
+import java.util.UUID;
+
+import javax.inject.Inject;
+
+import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fi.metatavu.kuntaapi.server.rest.model.Job;
+import fi.otavanopisto.kuntaapi.server.id.JobId;
+import fi.otavanopisto.kuntaapi.server.id.OrganizationId;
+import fi.otavanopisto.kuntaapi.server.integrations.KuntaApiConsts;
+import fi.otavanopisto.kuntaapi.server.integrations.tpt.TptConsts;
+import fi.otavanopisto.kuntaapi.server.integrations.tpt.TptTranslator;
+import fi.otavanopisto.kuntaapi.server.integrations.tpt.client.model.ApiResponse;
+import fi.otavanopisto.kuntaapi.server.integrations.tpt.client.model.DocsEntry;
+import fi.otavanopisto.kuntaapi.test.AbstractTest;
+
+@RunWith (CdiTestRunner.class)
+public class TptTranslatorTest extends AbstractTest {
+  
+  @Inject
+  private TptTranslator tptTranslator;
+
+  @Test
+  public void testNull() {
+    assertNotNull(tptTranslator);
+    tptTranslator.translateJob(getJobId(), null);
+  }
+
+  @Test
+  public void testHakuPaattyNull() throws IOException {
+    assertNotNull(tptTranslator);
+    ApiResponse areaSearch = loadAreaSearch();
+    
+    DocsEntry docsEntry = areaSearch.getResponse().getDocs().get(0);
+    assertNotNull(docsEntry);
+    assertNull(docsEntry.getHakuPaattyy());
+    
+    Job kuntaApiJob = tptTranslator.translateJob(getJobId(), docsEntry);
+    assertNotNull(kuntaApiJob);
+    assertNull(kuntaApiJob.getPublicationEnd());
+  }
+  
+  @Test
+  public void testHakuPaattyDate() throws IOException {
+    assertNotNull(tptTranslator);
+    ApiResponse areaSearch = loadAreaSearch();
+    
+    DocsEntry docsEntry = areaSearch.getResponse().getDocs().get(1);
+    assertNotNull(docsEntry);
+    assertEquals(docsEntry.getHakuPaattyy(), "14.04.2018");
+
+    Job kuntaApiJob = tptTranslator.translateJob(getJobId(), docsEntry);
+    assertNotNull(kuntaApiJob);
+    assertNotNull(kuntaApiJob.getPublicationEnd());
+    assertEquals(kuntaApiJob.getPublicationEnd().toInstant(), getInstant(2018, 4, 14, 0, 0, ZoneId.of(TptConsts.TIMEZONE)));    
+  }
+  
+  @Test
+  public void testHakuPaattyDateTime() throws IOException {
+    assertNotNull(tptTranslator);
+    ApiResponse areaSearch = loadAreaSearch();
+    
+    DocsEntry docsEntry = areaSearch.getResponse().getDocs().get(2);
+    assertNotNull(docsEntry);
+    assertEquals(docsEntry.getHakuPaattyy(), "15.05.2018 klo 14:00");
+
+    Job kuntaApiJob = tptTranslator.translateJob(getJobId(), docsEntry);
+    assertNotNull(kuntaApiJob);
+    assertNotNull(kuntaApiJob.getPublicationEnd());
+    assertEquals(kuntaApiJob.getPublicationEnd().toInstant(), getInstant(2018, 5, 15, 14, 0, ZoneId.of(TptConsts.TIMEZONE)));    
+  }
+
+  private JobId getJobId() {
+    return new JobId(getOrganizationId(), KuntaApiConsts.IDENTIFIER_NAME, UUID.randomUUID().toString());
+  }
+  
+  private OrganizationId getOrganizationId() {
+    return new OrganizationId(KuntaApiConsts.IDENTIFIER_NAME, UUID.randomUUID().toString());
+  }
+  
+  private ApiResponse loadAreaSearch() throws IOException {
+    try (InputStream fileStream = getClass().getClassLoader().getResourceAsStream("tpt/area-search.json")) {
+      ObjectMapper objectMapper = getObjectMapper();
+      return objectMapper.readValue(fileStream, new TypeReference<ApiResponse>() {});
+    }
+  }
+
+}
