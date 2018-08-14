edbd836d0d7ee960887db4f1780f4eca09728ab6
Antti Leppä <antti.leppa@metatavu.fi>
2018-08-14 05:55:00 +0300

    Updated organization tests to work with new PTV version and test data

--- src/test/java/fi/metatavu/kuntaapi/test/server/integration/ptv/OrganizationsTestsIT.java
+++ src/test/java/fi/metatavu/kuntaapi/test/server/integration/ptv/OrganizationsTestsIT.java
@@ -1,10 +1,12 @@
 package fi.metatavu.kuntaapi.test.server.integration.ptv;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
-import static org.hamcrest.core.IsNull.nullValue;
-import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+
+import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,16 +14,14 @@ import org.junit.Test;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.jayway.restassured.http.ContentType;
 
-import fi.metatavu.kuntaapi.test.AbstractIntegrationTest;
-
 @SuppressWarnings ("squid:S1192")
-public class OrganizationsTestsIT extends AbstractIntegrationTest {
+public class OrganizationsTestsIT extends AbstractPtvTest {
   
   /**
    * Starts WireMock
    */
   @Rule
-  public WireMockRule wireMockRule = new WireMockRule(getWireMockPort());
+  public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().port(getWireMockPort()), false);
   
   @Before
   public void beforeTest() throws InterruptedException {
@@ -31,149 +31,38 @@ public class OrganizationsTestsIT extends AbstractIntegrationTest {
     
     startMocks();
 
-    waitApiListCount("/services", TestPtvConsts.SERVICES.length);
-    waitApiListCount("/organizations", TestPtvConsts.ORGANIZATIONS.length);
+    waitApiListCount("/organizations", TestPtvConsts.ORGANIZATIONS.length); 
   }
 
   @Test
-  public void findOrganization() {
-    String id = givenReadonly()
+  public void findOrganization() throws InterruptedException, IOException, JSONException {
+    int organizationIndex = 2;
+    String organizationId = getOrganizationId(organizationIndex);
+    waitOrganizationServices(organizationIndex);
+    
+    String response = givenReadonly()
       .contentType(ContentType.JSON)
-      .get("/organizations")
-      .body().jsonPath().getString("id[2]");
-      
-    assertNotNull(id);
+      .get("/organizations/{organizationId}", organizationId)
+      .body().asString();
+    
+    assertJSONFileEquals(String.format("ptv/kuntaapi/organizations/%d.json", organizationIndex) , response, getOrganizationCustomizations());
+  }
+  
+  @Test
+  public void testListOrganizations() throws IOException {
+    int organizationIndex = 0;
+    waitOrganizationServices(organizationIndex);
     
     givenReadonly()
       .contentType(ContentType.JSON)
-      .get("/organizations/{organizationId}", id)
-      .then()
-      .assertThat()
-      .statusCode(200)
-      .body("id", is(id))
-      .body("municipality.code", is("507"))
-      .body("municipality.names.size()", is(2))
-      .body("municipality.names[0].language", is("sv"))
-      .body("municipality.names[0].value", is("Mäntyharju"))
-      .body("municipality.names[0].type", nullValue())
-      .body("organizationType", is("Municipality"))
-      .body("businessCode", is("0165761-0"))
-      .body("businessName", is("Mäntyharju"))
-      .body("names.size()", is(1))
-      .body("names[0].language", is("fi"))
-      .body("names[0].value", is("Mäntyharjun kunta"))
-      .body("names[0].type", is("Name"))
-      .body("displayNameType.size()", is(2))
-      .body("displayNameType[0].type", is("Name"))
-      .body("displayNameType[0].language", is("fi"))
-      .body("displayNameType[1].type", is("Name"))
-      .body("displayNameType[1].language", is("sv"))
-      .body("descriptions.size()", is(0))
-      .body("emailAddresses.size()", is(0))
-      .body("phoneNumbers.size()", is(0))
-      .body("webPages.size()", is(0))
-      .body("addresses.size()", is(0))
-      .body("publishingStatus", is("Published"))
-      .body("parentOrganization", nullValue())
-      .body("services.size()", is(0));
-  }
-  
-  @Test
-  public void testListOrganizations() {
-    givenReadonly()
-      .contentType(ContentType.JSON)
       .get("/organizations")
       .then()
       .assertThat()
       .statusCode(200)
-      .body("id[0]", notNullValue())
-      .body("municipality[0].code", is("491"))
-      .body("municipality[0].names.size()", is(2))
-      .body("municipality[0].names[0].language", is("sv"))
-      .body("municipality[0].names[0].value", is("S:t Michel"))
-      .body("municipality[0].names[0].type", nullValue())
-      .body("organizationType[0]", is("Municipality"))
-      .body("businessName[0]", is("Mikkelin kaupunki"))
-      .body("businessCode[0]", is("0165116-3"))
-      .body("names[0].size()", is(2))
-      .body("names[0][0].language", is("fi"))
-      .body("names[0][0].value", is("Mikkelin kaupunki"))
-      .body("names[0][0].type", is("Name"))
-      .body("displayNameType[0].size()", is(2))
-      .body("displayNameType[0][0].type", is("Name"))
-      .body("displayNameType[0][0].language", is("fi"))
-      .body("displayNameType[0][1].type", is("Name"))
-      .body("displayNameType[0][1].language", is("sv"))
-      
-      .body("descriptions[0].size()", is(1))
-      .body("descriptions[0][0].language", is("fi"))
-      .body("descriptions[0][0].value", is("Mikkeli on 55 000 asukkaan kaupunki Etelä-Savossa Saimaan rannalla. Asiointi Mikkelin kaupungin kanssa on sujuvaa. Palvelut ovat saatavilla verkossa ympäri vuorokauden. Asiointi onnistuu vaikka kotisohvalta käsin helposti ja nopeasti.\nMikkeli panostaa uudenlaisten palvelujen tuottamiseen ja tarjoamiseen. Avoin digitaalisuus palvelee kuntalaisia ja tarjoaa mallia koko valtakunnalle. \nAvoimen datan sivusto open.mikkeli.fi kutsuu kuntalaisia mukaan ideoimaan digitaalisuuden mahdollisuuksia."))
-      .body("descriptions[0][0].type", is("Description"))
-      .body("emailAddresses[0].size()", is(1))
-      .body("emailAddresses[0][0].language", is("fi"))
-      .body("emailAddresses[0][0].value", is("kirjaamo@mikkeli.fi"))
-      .body("emailAddresses[0][0].type", nullValue())
-      .body("phoneNumbers[0].size()", is(1))
-      .body("phoneNumbers[0][0].additionalInformation", nullValue())
-      .body("phoneNumbers[0][0].serviceChargeType", is("Charged"))
-      .body("phoneNumbers[0][0].chargeDescription", nullValue())
-      .body("phoneNumbers[0][0].prefixNumber", nullValue())
-      .body("phoneNumbers[0][0].isFinnishServiceNumber", is(Boolean.TRUE))
-      .body("phoneNumbers[0][0].number", is("0151941"))
-      .body("phoneNumbers[0][0].language", is("fi"))
-
-      .body("webPages[0].size()", is(1))
-      .body("webPages[0][0].description", nullValue())
-      .body("webPages[0][0].url", is("http://www.mikkeli.fi/"))
-      .body("webPages[0][0].language", is("fi"))
-      .body("webPages[0][0].value", is("Mikkelin kaupungin kotisivut"))
-      .body("webPages[0][0].type", nullValue())
+      .body("id.size()", is(3))
+      .body("[0]", jsonEqualsFile(String.format("ptv/kuntaapi/organizations/%d.json", organizationIndex), getOrganizationCustomizations()));
+  }
   
-      .body("addresses[0].size()", is(2))
-      .body("addresses[0][0].type", is("Postal"))
-      .body("addresses[0][0].subtype", is("PostOfficeBox"))
-      .body("addresses[0][0].postOfficeBox.size()", is(2))
-      .body("addresses[0][0].postOfficeBox[0].language", is("sv"))
-      .body("addresses[0][0].postOfficeBox[0].value", is("PL 33"))
-      .body("addresses[0][0].postOfficeBox[1].language", is("fi"))
-      .body("addresses[0][0].postOfficeBox[1].value", is("PL 33"))
-      .body("addresses[0][0].postalCode", is("50101"))
-      .body("addresses[0][0].postOffice.size()", is(2))
-      .body("addresses[0][0].postOffice[0].value", is("MIKKELI"))
-      .body("addresses[0][0].postOffice[0].language", is("sv"))
-      .body("addresses[0][0].streetAddress", nullValue())
-      .body("addresses[0][0].streetNumber", nullValue())
-      .body("addresses[0][0].municipality", nullValue())
-      .body("addresses[0][0].country", is("FI"))
-      .body("addresses[0][0].additionalInformations.size()", is(0))
-
-      .body("addresses[0][1].type", is("Visiting"))
-      .body("addresses[0][1].subtype", is("Street"))
-      .body("addresses[0][1].streetAddress.size()", is(1))
-      .body("addresses[0][1].streetAddress[0].value", is("Maaherrankatu 9-11"))
-      .body("addresses[0][1].streetAddress[0].language", is("fi"))
-      .body("addresses[0][1].streetNumber", nullValue())
-      .body("addresses[0][1].postalCode", is("50100"))
-      .body("addresses[0][1].municipality", nullValue())
-      .body("addresses[0][1].additionalInformations.size()", is(1))
-      .body("addresses[0][1].additionalInformations[0].value", is("Virastotalo"))
-      .body("addresses[0][1].additionalInformations[0].language", is("fi"))
-      .body("addresses[0][1].latitude", is("0"))
-      .body("addresses[0][1].longitude", is("0"))
-      .body("addresses[0][1].coordinateState", is("EmptyInputReceived"))
-      .body("addresses[0][1].country", is("FI"))
-      
-      .body("publishingStatus[0]", is("Published"))
-      .body("parentOrganization[0]", nullValue())
-      .body("services[0].size()", is(10))
-
-      .body("services[0][0].serviceId", notNullValue())
-      .body("services[0][0].organizationId", notNullValue())
-      .body("services[0][0].roleType", is("Responsible"))
-      .body("services[0][0].provisionType", nullValue())
-      .body("services[0][0].webPages.size()", is(0));
-  } 
-
   @Test
   public void testListOrganizationsByBusinessCode() {
     if (skipElasticSearchTests()) {
@@ -184,14 +73,14 @@ public class OrganizationsTestsIT extends AbstractIntegrationTest {
     
     givenReadonly()
       .contentType(ContentType.JSON)
-      .get("/organizations?businessCode=0165116-3")
+      .get("/organizations?businessCode=0209602-6")
       .then()
       .assertThat()
       .statusCode(200)
       .body("size()", is(1))
       .body("id[0]", notNullValue())
-      .body("businessName[0]", is("Mikkelin kaupunki"))
-      .body("businessCode[0]", is("0165116-3"));
+      .body("businessName[0]", is("Vaasa"))
+      .body("businessCode[0]", is("0209602-6"));
     
     givenReadonly()
       .contentType(ContentType.JSON)
@@ -212,14 +101,14 @@ public class OrganizationsTestsIT extends AbstractIntegrationTest {
     
     givenReadonly()
       .contentType(ContentType.JSON)
-      .get("/organizations?businessName=Mikkelin kaupunki")
+      .get("/organizations?businessName=Vaasa")
       .then()
       .assertThat()
       .statusCode(200)
       .body("size()", is(1))
       .body("id[0]", notNullValue())
-      .body("businessName[0]", is("Mikkelin kaupunki"))
-      .body("businessCode[0]", is("0165116-3"));
+      .body("businessName[0]", is("Vaasa"))
+      .body("businessCode[0]", is("0209602-6"));
     
     givenReadonly()
       .contentType(ContentType.JSON)
@@ -238,7 +127,7 @@ public class OrganizationsTestsIT extends AbstractIntegrationTest {
     
     waitForElasticIndex();
     
-    String search = "(M*)|(Mi*)";
+    String search = "(V*)|(Va*)";
     
     givenReadonly()
       .contentType(ContentType.JSON)
@@ -247,8 +136,8 @@ public class OrganizationsTestsIT extends AbstractIntegrationTest {
       .assertThat()
       .statusCode(200)
       .body("id.size()", is(2))
-      .body("names[0][0].value", is("Mikkelin kaupunki"))
-      .body("names[1][0].value", is("Mäntyharjun kunta"));
+      .body("businessName[0]", is("Vaasa"))
+      .body("businessName[1]", is("Väestörekisterikeskus"));
     
     givenReadonly()
       .contentType(ContentType.JSON)
@@ -257,8 +146,8 @@ public class OrganizationsTestsIT extends AbstractIntegrationTest {
       .assertThat()
       .statusCode(200)
       .body("id.size()", is(2))
-      .body("names[0][0].value", is("Mikkelin kaupunki"))
-      .body("names[1][0].value", is("Mäntyharjun kunta"));
+      .body("businessName[0]", is("Vaasa"))
+      .body("businessName[1]", is("Väestörekisterikeskus"));
     
     givenReadonly()
       .contentType(ContentType.JSON)
@@ -267,8 +156,8 @@ public class OrganizationsTestsIT extends AbstractIntegrationTest {
       .assertThat()
       .statusCode(200)
       .body("id.size()", is(2))
-      .body("names[0][0].value", is("Mäntyharjun kunta"))
-      .body("names[1][0].value", is("Mikkelin kaupunki"));
+      .body("businessName[0]", is("Väestörekisterikeskus"))
+      .body("businessName[1]", is("Vaasa"));
   }
 
 }
