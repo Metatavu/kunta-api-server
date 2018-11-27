c209e63ab7ac357a804c94d5e5afaa9e6747ad39
Antti Leppä <antti.leppa@metatavu.fi>
2018-11-27 10:43:27 +0200

    Changed test database charset into mb4

--- scripts/setup-db.sql
+++ scripts/setup-db.sql
@@ -1,4 +1,4 @@
 create user if not exists sa identified by 'sa';
 grant all on katest.* to sa; 
 drop database if exists katest; 
-create database katest default charset utf8;
+CREATE DATABASE katest /*!40100 DEFAULT CHARACTER SET utf8mb4 */
