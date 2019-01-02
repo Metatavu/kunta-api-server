3b858c0ece73fb825f8e4bfbc8765fd87582f5f3
Antti Leppä <antti.leppa@metatavu.fi>
2019-01-02 15:05:09 +0200

    Added default clients for docker build

--- docker/liquibase/settings.sql
+++ docker/liquibase/settings.sql
@@ -2,3 +2,4 @@ INSERT INTO SystemSetting (settingKey, value) VALUES ('elastic-search.hosts', 'e
 INSERT INTO SystemSetting (settingKey, value) VALUES ('ptv.baseUrl', 'https://api.palvelutietovaranto.trn.suomi.fi');
 INSERT INTO SystemSetting (settingKey, value) VALUES ('ptv.stsBaseUrl', 'https://sts.palvelutietovaranto.trn.suomi.fi');
 INSERT INTO SystemSetting (settingKey, value) VALUES ('ptv.authStrategy', 'FORM');
+INSERT INTO Client (name, clientId, clientSecret, accessType) VALUES ('readonly', 'READ_ONLY_ID', 'READ_ONLY_SECRET', 'READ_ONLY'), ('readwrite', 'READ_WRITE_ID', 'READ_WRITE_SECRET', 'READ_WRITE'), ('unrestricted', 'UNRESTRICTED_ID', 'UNRESTRICTED_SECRET', 'UNRESTRICTED');
