3a3af914dcaf70303a1539a8cce5f57a92339fe1
Antti Leppä <antti.leppa@metatavu.fi>
2019-01-10 19:20:21 +0200

    Changed docker image to wait for elastic search

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-echo "Wait for Elastic Search..." &&
-sleep 30
-curl http://elastic:9300
+echo "Wait for Elastic Search..."
+
+until $(curl -sSf -XGET --insecure 'http://elastic:9200/_cluster/health?wait_for_status=yellow' --silent --output /dev/null); do
+  printf '.'
+  sleep 1
+done
+
+echo "Elastic search ready."
 cd /opt/docker/liquibase &&
 echo "Applying database migrations..." &&
 /opt/liquibase/liquibase --contexts=production --changeLogFile=fi/metatavu/kuntaapi/server/liquibase/changelog.xml --url=jdbc:mariadb://${KUNTA_API_DB_HOST}:3306/${KUNTA_API_DB_NAME} --driver=org.mariadb.jdbc.Driver --classpath=/opt/jboss/wildfly/modules/system/layers/base/com/mariadb/jdbc/main/mariadb-java-client-2.3.0.jar --username=${KUNTA_API_DB_USER} --password=${KUNTA_API_DB_PASSWORD} update &&
