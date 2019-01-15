66cbb9ac7309505b47fba39e4fbee7e53d8f1c2f
Antti Leppä <antti.leppa@metatavu.fi>
2019-01-15 15:56:58 +0200

    Changed PTV service channel updates to use responding JMS queues

--- src/main/java/fi/metatavu/kuntaapi/server/integrations/ptv/tasks/ServiceChannelTasksQueue.java
+++ src/main/java/fi/metatavu/kuntaapi/server/integrations/ptv/tasks/ServiceChannelTasksQueue.java
@@ -1,11 +1,13 @@
 package fi.metatavu.kuntaapi.server.integrations.ptv.tasks;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.ApplicationScoped;
 
-import fi.metatavu.kuntaapi.server.tasks.jms.DefaultJmsTaskQueue;
+import fi.metatavu.kuntaapi.server.tasks.jms.AbstractJmsTaskQueue;
 
 @ApplicationScoped
-public class ServiceChannelTasksQueue extends DefaultJmsTaskQueue<AbstractServiceChannelTask> {
+public class ServiceChannelTasksQueue extends AbstractJmsTaskQueue<AbstractServiceChannelTask, Serializable> {
 
   public static final String NAME = "ptv-service-channels";
   public static final String JMS_QUEUE = JMS_QUEUE_PREFIX + NAME;
