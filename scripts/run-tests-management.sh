7b0b40513a3c0ce82210ebe8712fc0d88f6ecaba
Antti Leppä <antti.leppa@metatavu.fi>
2020-05-10 12:24:36 +0300

    tests

--- scripts/run-tests-management.sh
+++ scripts/run-tests-management.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$0/run-tests.sh AnnouncementTestsIT,BannerTestsIT,FragmentTestsIT,IncidentTestsIT,MenuTestsIT,PageTestsIT,PostTestsIT,ShortlinkTestsIT,TileTestsIT
+sh scripts/run-tests.sh AnnouncementTestsIT,BannerTestsIT,FragmentTestsIT,IncidentTestsIT,MenuTestsIT,PageTestsIT,PostTestsIT,ShortlinkTestsIT,TileTestsIT
